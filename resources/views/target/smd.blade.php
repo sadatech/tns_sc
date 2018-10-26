6fb56bc9ec3294b034ae7d3e92a526dbca99baa2
ExcelOkta <excelokta@gmail.com>
2018-10-26 10:43:10 +0700

    fix target modal

--- resources/views/target/smd.blade.php
+++ resources/views/target/smd.blade.php
@@ -105,7 +105,7 @@
     <div class="modal-content">
       <div class="block block-themed block-transparent mb-0">
         <div class="block-header bg-primary p-10">
-          <h3 class="block-title"><i class="fa fa-plus"></i> Add Product Fokus</h3>
+          <h3 class="block-title"><i class="fa fa-plus"></i> Add Target</h3>
           <div class="block-options">
             <button type="button" class="btn-block-option" data-dismiss="modal" aria-label="Close">
               <i class="si si-close"></i>
