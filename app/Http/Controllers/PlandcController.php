3ac744e139967f682142c9da317245ab29fd89a8
bennywij99 <bennywij99@gmail.com>
2018-10-26 10:28:40 +0700

    plan:

--- app/Http/Controllers/PlandcController.php
+++ app/Http/Controllers/PlandcController.php
@@ -23,4 +23,92 @@ class PlandcController extends Controller
             <button data-url=".route('plan.delete', $plan->id)." class='btn btn-sm btn-danger btn-square js-swal-delete' title='Delete'><i class='si si-trash'></i></button>";
         })->make(true);
     }
+
+    public function import(Request $request)
+    {
+        $this->validate($request, [
+            'file' =>   'required|'
+        ]);
+        $transaction = DB::transaction(function () use ($request) {
+            $file = Input::file('file')->getClientOriginalName();
+            $filename = pathinfo($file, PATHINFO_FILENAME);
+            $extension = pathinfo($file, PATHINFO_EXTENSION);
+
+            if ($extension != 'xlsx' && $extension !=  'xls') {
+                return response()->json(['error' => 'true', 'error_detail' => "Error File Extention ($extension)"]);
+            }
+            if($request->hasFile('file')){
+                $file = $request->file('file')->getRealPath();
+                $ext = '';
+                
+                Excel::filter('chunk')->selectSheetsByIndex(0)->load($file)->chunk(250, function($results) use ($id_company)
+                {
+                    foreach($results as $row)
+                    {
+                        echo "$row<hr>";
+                        $insert = PlanDc::create([
+                            'date'              => $row->date,
+                            'lokasi'            => $row->lokasi,
+                            'stocklist'         => $row->stocklist
+                        ]);
+                        if ($insert) 
+                            {
+                                $dataDc = array();
+                                foreach ($request->input('employee') as $emp) {
+                                    $dataDc[] = array(
+                                        'id_employee'       => $emp,
+                                        'id_plandc'         => $insert->id
+                                    );
+                                }
+                                DB::table('pland_dc')->insert($dataDc); 
+                            }
+                        // if ($insert) {
+                        //     $dataPlan = array();
+                        //     $listPlan = explode(",", $row->distributor);
+                        //     foreach ($listPlan as $plan) {
+                        //         $dataPlan[] = array(
+                        //             'id_employee'       => $this->findEmployee($plan),
+                        //             'id_plandc'         => $insert->id
+                        //         );
+                        //     }
+                        //     DB::table('plan_employees')->insert($dataPlan);
+                        // }
+                    }
+                },false);
+            }
+            return 'success';
+        });
+
+        if ($transaction == 'success') {
+            return redirect()->back()
+            ->with([
+                'type'      => 'success',
+                'title'     => 'Sukses!<br/>',
+                'message'   => '<i class="em em-confetti_ball mr-2"></i>Berhasil import!'
+            ]);
+        }else{
+            return redirect()->back()
+            ->with([
+                'type'    => 'danger',
+                'title'   => 'Gagal!<br/>',
+                'message' => '<i class="em em-warning mr-2"></i>Gagal import!'
+            ]);
+        }
+    }
+
+    // public function findEmployee($data)
+    // {
+    //     $dataEmp = Employee::whereRaw("TRIM(UPPER(name)) = '". trim(strtoupper($data))."'");
+    //     if ($dataEmp->count() == 0) {
+    //         $Emp = Distributor::create([
+    //             'name'       => $data,
+    //         ]);
+    //         if ($Emp) {
+    //             $id_Emp = $Emp->id;
+    //         }
+    //     } else {
+    //         $id_Emp = $dataEmp->first()->id;
+    //     }
+    //     return $id_Emp;
+    // }
 }
