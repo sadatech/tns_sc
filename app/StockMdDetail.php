e58cd50a334e9a4d09b9d5a28d622a2c439d0c13
ExcelOkta <excelokta@gmail.com>
2018-11-02 15:15:53 +0700

    Add stock api

--- app/StockMdDetail.php
+++ app/StockMdDetail.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class StockMdDetail extends Model
 {
-    //
+    protected $fillable = [
+        'id_stock', 'id_product', 'id_satuan'
+    ];
+
+    public function stock()
+    {
+        return $this->belongsTo('App\StockMdHeader', 'id_stock');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo('App\Product', 'id_product');
+    }
 }
