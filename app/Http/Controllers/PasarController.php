f52cdd28c6e9bbb02f80b0fe5b07a2df1f5e3bb9
bennywij99 <bennywij99@gmail.com>
2018-11-06 17:00:11 +0700

    fix latitudelongitude import pasar

--- app/Http/Controllers/PasarController.php
+++ app/Http/Controllers/PasarController.php
@@ -181,7 +181,7 @@ class PasarController extends Controller
                             Pasar::create([
                                 'name'              => $row->pasar,
                                 'id_subarea'        => $id_subarea,
-                                'longitude'         => (isset($row->longitude)? $row->longitude : "-"),
+                                'longitude'         => (isset($row->longitude) ? $row->longitude : "-"),
                                 'latitude'          => (isset($row->latitude) ? $row->latitude : "-"),
                                 'address'           => $row->address
                             ])->id;
@@ -213,19 +213,19 @@ class PasarController extends Controller
 
     public function findSub($data)
     {
-        $dataSub = Subarea::whereRaw("TRIM(UPPER(name)) = '". trim(strtoupper($data['subarea_name']))."'");
-        if ($dataSub->count() < 1 ) {
-
+        $dataSub1 = SubArea::whereRaw("TRIM(UPPER(name)) = '". trim(strtoupper($data['subarea_name']))."'");
+        if ($dataSub1->count() < 1 ) {
+            
             $dataSub['area_name']  = $data['area_name'];
             $dataSub['region_name']  = $data['region_name'];
             $id_area = $this->findArea($dataSub);
-            $subarea = Subarea::create([
+            $subarea = SubArea::create([
               'name'        => $data['subarea_name'],
               'id_area'     => $id_area
           ]);
             $id_subarea = $subarea->id;
         }else{
-            $id_subarea = $dataSub->first()->id;
+            $id_subarea = $dataSub1->first()->id;
         }
         return $id_subarea;
     }
