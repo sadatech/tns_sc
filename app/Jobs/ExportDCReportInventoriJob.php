d830d5ceac26448d8b1c4c5f766fa1ac3e7d0e1e
Andika Muhammad Cahya <andika@wannadream.tech>
2018-12-12 00:27:54 +0700

    DC Report Inventori Filter, Export Queue

---
+++ app/Jobs/ExportDCReportInventoriJob.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+use App\JobTrace;
+use App\Traits\ExportDCReportInventoriTrait;
+
+class ExportDCReportInventoriJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, ExportDCReportInventoriTrait;
+
+    protected $trace;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 5;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(JobTrace $trace)
+    {
+        $this->trace = $trace;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->trace->update([
+            'status' => 'DONE',
+            'results' => $this->DCReportInventoriExportTrait(), // return excel file location
+        ]);
+    }
+
+    /**
+     * The job failed to process.
+     *
+     * @param  Exception  $exception
+     * @return void
+     */
+    public function failed(Exception $exception)
+    {
+        return $this->trace->update([
+            'status' => 'FAILED',
+            'log' => $exception->getMessage(),
+        ]);
+    }
+
+}
